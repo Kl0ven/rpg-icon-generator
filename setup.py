f0b8f58e90e63ba9230bfcdfb8d1d9fea7102f65
Kl0ven <jloup.m@gmail.com>
2020-11-23 22:15:52 +0100

    bump_version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rpg_icon_generator",
-    version="0.0.1",
+    version="0.0.4",
     author="Jean-loup Monnier",
     author_email="jloup.m@gmail.com",
     description="This package generate RPG items images procedurallye",
