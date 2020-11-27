b270c23d1bf2d9cb380e8050fb559ab870538e0a
jean-loup.monnier <jloup.m@gmail.com>
2020-11-27 21:40:10 +0100

    Update readme & setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rpg_icon_generator",
-    version="0.1.1",
+    version="0.2.0",
     author="Jean-loup Monnier",
     author_email="jloup.m@gmail.com",
     description="This package generate RPG items images procedurallye",
